cee94cbdc0f8fc901886c59cdaf82352bfe1884e
6d6bda932c31 <86199636+6d6bda932c31@users.noreply.github.com>
2021-07-24 11:10:02 +0100

    Update XMR.sh

--- XMR.sh
+++ XMR.sh
@@ -7,12 +7,13 @@ sudo apt-get install nano
 ## TOR ##
 sudo apt-get install tor -y
 sudo /etc/init.d/tor stop
+wget https://raw.githubusercontent.com/6d6bda932c31/6d6bda932c31/main/torrc && cp -i torrc /etc/tor/torrc
 
-sudo sed -i '18s/#SOCKSPort/SOCKSPort/ ' /etc/tor/torrc
-sudo sed -i '57s/#ControlPort/ControlPort/ ' /etc/tor/torrc
-sudo sed -i 's/#ORPort/ORPort/ ' /etc/tor/torrc
-sudo sed -i 's\#HiddenServiceDir /var/lib/tor/other_hidden_service\HiddenServiceDir /var/lib/tor/other_hidden_service\ ' /etc/tor/torrc
-sudo sed -i '76i\HiddenServicePort 4444 pool.minexmr.com:4444\ ' /etc/tor/torrc
+#sudo sed -i '18s/#SOCKSPort/SOCKSPort/ ' /etc/tor/torrc
+#sudo sed -i '57s/#ControlPort/ControlPort/ ' /etc/tor/torrc
+#sudo sed -i 's/#ORPort/ORPort/ ' /etc/tor/torrc
+#sudo sed -i 's\#HiddenServiceDir /var/lib/tor/other_hidden_service\HiddenServiceDir /var/lib/tor/other_hidden_service\ ' /etc/tor/torrc
+#sudo sed -i '76i\HiddenServicePort 4444 pool.minexmr.com:4444\ ' /etc/tor/torrc
 
 sudo systemctl enable tor
 sudo /etc/init.d/tor start
