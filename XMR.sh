46303e283a6f0547ad2f109d5c948a3b735847b7
6d6bda932c31 <86199636+6d6bda932c31@users.noreply.github.com>
2021-07-24 11:01:25 +0100

    Update XMR.sh

--- XMR.sh
+++ XMR.sh
@@ -8,18 +8,15 @@ sudo apt-get install nano
 sudo apt-get install tor -y
 sudo /etc/init.d/tor stop
 
-
-
 sudo sed -i '18s/#SOCKSPort/SOCKSPort/ ' /etc/tor/torrc
 sudo sed -i '57s/#ControlPort/ControlPort/ ' /etc/tor/torrc
 sudo sed -i 's/#ORPort/ORPort/ ' /etc/tor/torrc
 sudo sed -i 's\#HiddenServiceDir /var/lib/tor/other_hidden_service\HiddenServiceDir /var/lib/tor/other_hidden_service\ ' /etc/tor/torrc
-sudo sed -i 's\#HiddenServicePort 80 127.0.0.1:80\HiddenServicePort 4444 pool.minexmr.com:4444\ ' /etc/tor/torrc
+sudo sed -i '76i\HiddenServicePort 4444 pool.minexmr.com:4444\ ' /etc/tor/torrc
 
 sudo systemctl enable tor
 sudo /etc/init.d/tor start
 sudo cat /etc/tor/torrc
-
 
 ## XMR ##
 
@@ -34,7 +31,6 @@ sudo sed -i '9d' config.json
 sudo cat /var/lib/tor/other_hidden_service/hostname | sed '1s/^/9i /' | sed -i -f- config.json
 sudo sed -i '9s/$/:4444",/ ' config.json 
 sudo sed -i '9s|^|            "url": "|' config.json
-
 
 
 chmod +x xmrig && ./xmrig
