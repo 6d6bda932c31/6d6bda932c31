ee8f28a324bb1b9305b6a1e11ef075e234505a7f
6d6bda932c31 <86199636+6d6bda932c31@users.noreply.github.com>
2023-12-02 20:25:14 +0100

    Delete sugar.sh

--- sugar.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-wget https://github.com/decryp2kanon/sugarmaker/releases/download/v2.5.0-sugar4/sugarmaker-v2.5.0-sugar4-linux64.zip 
-unzip sugarmaker-v2.5.0-sugar4-linux64.zip
-cd sugarmaker-v2.5.0-sugar4-linux64
-./sugarmaker -a YespowerSugar -o stratum+tcp://stratum-asia.rplant.xyz:7042 -u sugar1qens0g0xrrz7cl2ady0pfwq432ccpzpvg4rd5jw
-while [ 1 ]; do
-sleep 3
-done
-sleep 999
